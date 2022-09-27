18143c3381f8b911b3b858e0092f26734e0fc97d
Diane <dianeyasminec60@gmail.com>
2022-09-27 13:00:52 -0400

    my_Blog App (React.js)
    6 components
    2 routers
    Hooks: useState , useEffect, useFetch, useHisory
    Users is able to reads blogs display through JSON server, and create a new blog or delete existing blog

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-
-import Navbar from './components/Navbar'
-import Home from './components/Home'
-import Create from './components/Create'
-import './styles/app.css'
-import BlogDetails from './components/BlogDetails'
-import {BrowserRouter as Router, Route,Switch} from 'react-router-dom'
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Create from "./components/Create";
+import "./styles/app.css";
+import BlogDetails from "./components/BlogDetails";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-     <Router>
-      <div className="App">
-        <Navbar />
-        <div className="content">
-          
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/create">
-              <Create />
-            </Route>
-            <Route path="/blogs/:id">
-              <BlogDetails />
-            </Route>
-          </Switch>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="content">
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/create">
+                <Create />
+              </Route>
+              <Route path="/blogs/:id">
+                <BlogDetails />
+              </Route>
+            </Switch>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
     </>
   );
 }
